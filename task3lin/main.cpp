08b713d89c07312f3397a4fcba5215e22bd3d9c9
paul-eduard <eduard.paul.94@gmail.com>
2014-11-28 13:22:32 +0300

    Final OMP version

--- task3lin/main.cpp
+++ task3lin/main.cpp
@@ -37,13 +37,13 @@ void double_LSD_Radix_Sort(double *a_, int left, int right){
     double *b2 = new double[right-left+1];
 
     int q = 0, q2 = 0;
-        for(int i=left; i<right+1; i++)
-            if(doubleMask(63)==(doubleMask(63)&(a[i]))) 
-                b1[q++]=a_[i];
-            else
-                b2[q2++]=a_[i];
-        for(int i=0;i<q;i++) a_[i+left]=b1[i];
-        for(int i=0;i<q2;i++) a_[q+i+left]=b2[i];
+    for(int i=left; i<right+1; i++)
+        if(doubleMask(63)==(doubleMask(63)&(a[i]))) 
+            b1[q++]=a_[i];
+        else
+            b2[q2++]=a_[i];
+    for(int i=0;i<q;i++) a_[i+left]=b1[i];
+    for(int i=0;i<q2;i++) a_[q+i+left]=b2[i];
 
     while(numBit < 63){
         int q1 = 0, q2 = 0;
@@ -144,22 +144,96 @@ void double_MSD_Radix_Sort(double *a_, int left, int right){
     _double_MSD_Radix_Sort(a,left,r,1);
 }
 
-void merge(double *a, double *b1, int k1, int k2, int k3) {
-    int k=k1, kk=k2;
-            while((k1<kk)&&(k2<k3+1)) {
-                if(a[k1]<=a[k2]){
-                    b1[k++]=a[k1++];
-                } else {
-                    b1[k++]=a[k2++];
-                }
-            }
-            while (k1<kk) b1[k++]=a[k1++];
-            while (k2<k3+1) b1[k++]=a[k2++];
+//void merge(double *a, double *b1, int _k1, int _k2, int _k3) {
+//    int k1=_k1, k2=_k2, k=k1, kk=k2;
+//
+//    while((k1<kk)&&(k2<_k3+1)) {
+//        if(a[k1]<=a[k2]){
+//            b1[k++]=a[k1++];
+//        } else {
+//            b1[k++]=a[k2++];
+//        }
+//    }
+//    while (k1<kk) b1[k++]=a[k1++];
+//    while (k2<_k3+1) b1[k++]=a[k2++];
+//    for(int i = _k1;i<_k3+1;i++) a[i]=b1[i];
+//}
+
+void merge(double *a, int _k1, int _k2, int _k3) {
+    int k1=_k1, k2=_k2, k=0, kk=k2;
+    double *b1 = new double[_k3-_k1+1];
+    while((k1<kk)&&(k2<_k3+1)) {
+        if(a[k1]<=a[k2]){
+            b1[k++]=a[k1++];
+        } else {
+            b1[k++]=a[k2++];
+        }
+    }
+    while (k1<kk) b1[k++]=a[k1++];
+    while (k2<_k3+1) b1[k++]=a[k2++];
+    for(int i = _k1;i<_k3+1;i++) a[i]=b1[i-_k1];
+    delete[] b1;
+}
+
+void localSort(double* a, int N, int threadNum=1){
+    omp_set_dynamic(0); 
+    omp_set_num_threads(threadNum);
+#pragma omp parallel
+    {
+        int q = omp_get_thread_num();
+        int numCount = N/omp_get_num_threads() + 1;
+        double_MSD_Radix_Sort(a,__min(q*numCount,N-1),__min((1+q)*numCount-1,N-1));
+#pragma omp barrier 
+#pragma omp master 
+        if (omp_get_num_threads()==2) {
+            int k = N/omp_get_num_threads() + 1;
+            merge(a,0,k,N-1);
+        }
+        if (omp_get_num_threads()==4) {
+            int k2 = N/omp_get_num_threads() + 1, k3 = 2*(N/omp_get_num_threads()+1),
+                k4 = 3*(N/omp_get_num_threads() + 1);
+#pragma omp single 
+            merge(a,0,k2,k3-1);
+#pragma omp single 
+            merge(a,k3,k4,N-1);
+#pragma omp barrier 
+#pragma omp master 
+            merge(a,0,k3,N-1);
+        }
+    } 
+}
+
+void localParSort(double* a, int N, int threadNum=1){
+    omp_set_dynamic(0); 
+    omp_set_num_threads(threadNum);
+#pragma omp parallel
+    {
+        int q = omp_get_thread_num();
+        int numCount = N/omp_get_num_threads() + 1;
+        double_MSD_Radix_Sort(a,__min(q*numCount,N-1),__min((1+q)*numCount-1,N-1));
+#pragma omp barrier 
+#pragma omp master 
+        if (omp_get_num_threads()==2) {
+            int k = N/omp_get_num_threads() + 1;
+            merge(a,0,k,N-1);
+        }
+        if (omp_get_num_threads()==4) {
+            int k2 = N/omp_get_num_threads() + 1, k3 = 2*(N/omp_get_num_threads()+1),
+                k4 = 3*(N/omp_get_num_threads() + 1);
+#pragma omp single 
+            merge(a,0,k2,k3-1);
+#pragma omp single 
+            merge(a,k3,k4,N-1);
+#pragma omp barrier 
+#pragma omp master 
+            merge(a,0,k3,N-1);
+        }
+    } 
 }
 
 int main(int argc, char **argv)
 {
-    int N = 10;
+    int N = 10000000;
     double *a = new double[N];
     for(int i=0;i<N;i++){
         a[i]=N/2-i;
@@ -168,34 +242,9 @@ int main(int argc, char **argv)
     for(int i=0;i<N;i++){
         b[i]=N/2-i;
     }
-
-    double *b1 = new double[N];
-    omp_set_dynamic(0); 
-    omp_set_num_threads(2);
+   
     double time1=-omp_get_wtime();
-#pragma omp parallel
-{
-        int q = omp_get_thread_num();
-        int numCount = N/omp_get_num_threads() + 1;
-        double_MSD_Radix_Sort(a,__min(q*numCount,N-1),__min((1+q)*numCount-1,N-1));
-#pragma omp barrier 
-#pragma omp master 
-        if (omp_get_num_threads()==2) {
-            int k = N/omp_get_num_threads() + 1;
-            merge(a,b1,0,k,N-1);
-        }
-        if (omp_get_num_threads()==4) {
-            int k2 = N/omp_get_num_threads() + 1, k3 = 2*(N/omp_get_num_threads()+1),
-                k4 = 3*(N/omp_get_num_threads() + 1);
-#pragma omp single 
-            merge(a,b1,0,k2,k3-1);
-#pragma omp single 
-            merge(a,b1,k3,k4,N-1);
-#pragma omp barrier 
-#pragma omp master 
-            merge(b1,a,0,k3,N-1);
-        }
-}   
+    localParSort(a,N,4);
     time1+=omp_get_wtime();
     std::cout<<time1<<std::endl;
 
@@ -204,11 +253,13 @@ int main(int argc, char **argv)
     time2+=omp_get_wtime();
     std::cout<<time2<<std::endl;
 
-    for(int i=0;i<N;i++){
-        std::cout<<a[i]<<", ";
-    }std::cout<<std::endl;
-    for(int i=0;i<N;i++){
-        std::cout<<b1[i]<<", ";
+    if(N<30){
+        for(int i=0;i<N;i++){
+            std::cout<<a[i]<<", ";
+        }std::cout<<std::endl;
+        for(int i=0;i<N;i++){
+            std::cout<<b[i]<<", ";
+        }
     }
     system("pause");
     return 0;
